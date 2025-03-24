7e948332cb17eb1aa10d457c89457ae461aa9889
ambik <kdhakshinamoorthy@csus.edu>
2025-03-23 18:15:39 -0700

    Working Image Slider with toggle bar with bug in image display.

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,7 @@
 import { Text, View, StyleSheet, ScrollView, ImageBackground } from "react-native";
 import ContactsIcons from "./sections/contact-section/contacts-icons";
 import EventsPage from "./sections/EventsPage/events-page";
-
-
-
+import ImageSlider from "./sections/ImageSlider/Image_Slider";
 
 export default function Index() {
   const styles = StyleSheet.create({
@@ -16,6 +14,7 @@ export default function Index() {
   });
   return (
     <View style={styles.container}>
+      <ImageSlider></ImageSlider>
       <ContactsIcons></ContactsIcons> 
       <EventsPage></EventsPage>
     </View>
