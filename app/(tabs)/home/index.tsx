41604dc167e0fa882c0cdc63700e0b114ed857d3
Tarrin Gackstetter <tsgackstetter@csus.edu>
2025-04-18 11:32:56 -0700

    Copy current landing page to home

---
+++ app/(tabs)/home/index.tsx
@@ -0,0 +1,34 @@
+import { View, StyleSheet, ScrollView } from "react-native";
+import ContactsIcons from "../components/universal/contacts-icons";
+import DynamicEventSection from "../components/home/dynamic-event";
+import Resources from "../components/home/resources";
+import HeaderBar from "../components/universal/header-bar";
+import ImageSlider from "../components/home/Image_slider";
+import MediaPage from "./(tabs)/media"
+import DynamicButton from "@/components/universal/dynamic-button";
+
+
+export default function Index() {
+  const styles = StyleSheet.create({
+    scrollContainer: {
+      flex: 1,
+      backgroundColor: '#fff',
+    },
+    container: {
+      alignItems: 'center'
+    },
+  });
+  return (
+    <ScrollView style = {styles.scrollContainer}>
+      <HeaderBar />
+      <ImageSlider></ImageSlider>
+      <View style={styles.container}>
+        {/*<MediaPage></MediaPage>*/}
+        {/*<EventsPage></EventsPage>*/}
+        <DynamicEventSection></DynamicEventSection>
+        <ContactsIcons></ContactsIcons> 
+        <Resources></Resources>
+      </View>
+    </ScrollView>
+  );
+}
