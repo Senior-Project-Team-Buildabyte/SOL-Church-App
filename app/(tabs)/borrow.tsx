c22dc23f761dfc9936b24b02d03f994a4c6fa01d
dylank202 <dkeener@csus.edu>
2025-09-12 10:41:08 -0700

    Created borrow tab landing page

--- app/(tabs)/borrow.tsx
+++ app/(tabs)/borrow.tsx
@@ -1,10 +1,123 @@
-import { Text } from "react-native";
+import DynamicButton from "@/components/universal/dynamic-button";
+import { LinearGradient } from "expo-linear-gradient";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
 
 
 const BorrowPage = () => {
     return (
-        <Text>Borrow Page</Text>
-    );
+    
+    <View style={styles.container}>
+        <ImageBackground
+        source={require('@/assets/images/bg-mission.jpg')}
+        style={styles.frontimage}
+        resizeMode="cover">
+
+            <View style={styles.headingWrapper}>
+                <LinearGradient
+                    style={[ styles.pageHeading ]}
+                    colors={["rgba(149, 185, 247, 1)", "rgba(60,129,246,1)"]}
+                    {...{
+                    start: { x: 0, y: 0.5 },
+                    end: { x: 1, y: 0.5 },    
+                    }}
+                >
+                    <ImageBackground
+                    source={require('@/assets/images/favicon-drop.png')}
+                    style={ [{height: '100%'}, {aspectRatio: 1},] }
+                    resizeMode="cover">
+                    </ImageBackground>
+
+                    <Text style={styles.headingText}>SOL Inventory</Text>
+
+                </LinearGradient>
+                {/* <View style={styles.pageHeading}>
+
+                </View> */}
+            </View>
+
+        </ImageBackground>
+        
+        <View style={styles.buttons}>
+
+            <DynamicButton buttons={[
+                {
+                    type: 1, // internal
+                    shape: 0, // full
+                    buttonConfig: {
+                        text: "Borrow an Item",
+                        icon: "shopping-basket",
+                        internalLink: `../borrow`,
+                        backgroundGradient: ["0", "rgba(60,129,246,1)", "rgba(149, 185, 247, 1)"]
+                    }
+                },
+            ]}/>
+
+            <DynamicButton buttons={[
+                {
+                    type: 1, // internal
+                    shape: 0, // full
+                    buttonConfig: {
+                        text: "Return Items",
+                        icon: "undo",
+                        internalLink: `../borrow`,
+                        backgroundGradient: ["0", "rgba(149, 185, 247, 1)", "rgba(60,129,246,1)"]
+                    }
+                },
+            ]}/>
+
+        </View>
+    </View>
+
+    )
 };
+
+
+// styles
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#fff',
+        width: '100%',
+        height: '100%',
+    },
+    frontimage: {
+        width: '100%',
+        height: 250,
+    },
+    headingWrapper: {
+        maxWidth: 650,
+        width: '100%',
+        height: '100%',
+        alignSelf: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    pageHeading: {
+        height: 60,
+        backgroundColor: 'rgba(58, 120, 227, 1)',
+        width: '70%',
+        maxWidth: 350,
+        borderRadius: 100,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+    },
+    headingText: {
+        color: '#fff',
+        fontSize: 30,
+        textAlign: 'center',
+        flexGrow: 1,
+        fontWeight: 'bold',
+        textShadowColor: '#333',
+        textShadowRadius: 10,
+        textShadowOffset: {width: 1, height: 2},
+    },
+    buttons: {
+        flex: 1,
+        justifyContent: 'space-evenly',
+        maxHeight: 500,
+        width: '90%',
+        alignSelf: 'center'
+    }
+});
 
 export default BorrowPage;
