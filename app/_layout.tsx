62682da1cf917776f6d342a0a4a9044691a0531b
dylank202 <dkeener@csus.edu>
2025-09-27 01:10:32 -0700

    Fix header safe area on web + mobile and prevent duplicate navigation to account page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,18 @@
-import { Stack } from "expo-router";
+import { Slot, Stack } from "expo-router";
 import HeaderBar from '../components/universal/header-bar';
+import { initialWindowMetrics, SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { View } from "react-native";
 
 export default function RootLayout() {
   return (
-    <>
-      <HeaderBar />
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
-    </>
+    <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+      <SafeAreaView edges={['top', 'left', 'right']} style={{ backgroundColor: 'white' }}>
+        <HeaderBar />
+      </SafeAreaView>
+
+      <View style={{ flex: 1 }}>
+        <Stack screenOptions={{ headerShown: false }} />
+      </View>
+    </SafeAreaProvider>
   );
 }
